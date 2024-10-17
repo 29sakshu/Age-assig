a8d38fa349ce160e7a139c56986b9ebdba2b11c7
29sakshu <sakshis29091@gmail.com>
2024-10-17 19:48:47 +0300

    Retirement condition added

--- src/AgeApp.java
+++ src/AgeApp.java
@@ -1,12 +1,13 @@
 public class AgeApp {
     public static void main(String[] args) {
-
-int age = 25; 
-if (age > 0 && age < 18) { 
-    System.out.println("You are underage.");
- } else { 
-    System.out.println("You are an adult.");
- } 
-System.out.println("Press space to exit.");
+        int age = 70; 
+        if (age > 0 && age < 18) {
+            System.out.println("You are underage.");
+        } else if (age >= 65) {
+            System.out.println("You are retired.");
+        } else {
+            System.out.println("You are an adult.");
+        }
+        System.out.println("Press space to exit.");
     }
-} 
+}
